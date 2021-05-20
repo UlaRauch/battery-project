5894714c88a1081f6bb0f3d810feb94f6acb6c26
UlaRauch <71543978+UlaRauch@users.noreply.github.com>
2021-05-20 09:43:33 +0200

    signUpIn.html signUpIn.css signUpIn.js <- neu, nicht fertig

--- JS/signUpIn.js
+++ JS/signUpIn.js
@@ -1,3 +1,26 @@
+function openSignUpInForm(evt, signInMode) {
+    var i, tabcontent, tablinks;
+
+    //anpassen, index von W3schools übernommen aber macht für 2 tabs wenig sinn
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("signupin_tab-group");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+
+    document.getElementById(signInMode).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+
+/*
+//code von https://www.templatemonster.com/blog/html5-css3-registration-forms/
+für übungszwecke übernommen, funktioniert aktuell nicht
 $('.tab a').on('click', function (e) {
 
     e.preventDefault();
@@ -12,3 +35,4 @@ $('.tab a').on('click', function (e) {
     $(target).fadeIn(600);
 
 });
+*/
