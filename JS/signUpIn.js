011d3124698994497161810e351d3ccd4c3cc504
UlaRauch <71543978+UlaRauch@users.noreply.github.com>
2021-06-19 20:30:12 +0200

    tabs sind aktuell unsichtbar, funktioniert nicht.

--- JS/signUpIn.js
+++ JS/signUpIn.js
@@ -1,21 +1,45 @@
-function openSignUpInForm(tab_id, tabcontent_id) {
-    var i, tabcontent, tablinks;
+document.addEventListener("DOMContentLoaded", function (event) {
 
-    //anpassen, index von W3schools übernommen aber macht für 2 tabs wenig sinn
-    tabcontent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+    //create goToSignIn button
+    let tabButtonSignIn = document.createElement("button");
+    document.getElementById(goToSignIn).appendChild(tabButtonSignIn);
+    let buttonTextSignIn = document.createTextNode("Sign In");
+    tabButtonSignIn.appendChild(buttonTextSignIn);
+    tabButtonSignIn.addEventListener("click", function () {openSignUpInForm(goToSignIn, signinform)})
+
+    //create goToSignUp button
+    let tabButtonSignUp = document.createElement("button");
+    document.getElementById(goToSignUp).appendChild(tabButtonSignUp);
+    let buttonTextSignUp = document.createTextNode("Sign Up");
+    tabButtonSignUp.appendChild(buttonTextSignUp);
+    tabButtonSignUp.addEventListener("click", function () {openSignUpInForm(goToSignUp, signupform)});
+
+    /*
+    tutorial: https://rudrastyh.com/javascript/tabs.html
+     */
+    function openSignUpInForm(tab_id, tabcontent_id) {
+
+        let i;
+        let tabcontent; //x
+        let tablinks;
+
+        //anpassen, index von W3schools übernommen aber macht für 2 tabs wenig sinn
+        tabcontent = document.getElementsByClassName("tab-content");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = 'none';
+        }
+
+        document.getElementById(tabcontent_id).style.display = "block";
+
+
+        tablinks = document.getElementsByClassName("tab");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = 'tab';
+        }
+        document.getElementById(tab_id).className = 'tab active';
     }
 
-    document.getElementById(tabcontent_id).style.display = "block";
-
-
-    tablinks = document.getElementsByClassName("tab");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className = 'tab';
-    }
-    document.getElementById(tab_id).className = 'tab active';
-}
+});
 
 /*
 //code von https://www.templatemonster.com/blog/html5-css3-registration-forms/
