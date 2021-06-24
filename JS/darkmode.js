ce0cc0e228ae6cc6eca74f341795c8299e28643c
Mattias <42864156+Mattias-R@users.noreply.github.com>
2021-06-24 11:28:31 +0200

    implemented darkmode :)

--- JS/darkmode.js
+++ JS/darkmode.js
@@ -1,18 +1,28 @@
 
-    const darkModeToggle = null;
+    const darkModeToggle = document.getElementById("dark-mode-toggle");
 
     let darkMode = localStorage.getItem('darkMode');
 
     const enableDarkMode = () => {
         localStorage.setItem("darkMode", "enabled");
+        let x = document.getElementById("easterEgg");
+        x.className = "test";
+        //x.style.backgroundColor = "red";
     }
 
     const disableDarkMode = () => {
         localStorage.setItem("darkMode", null);
+        let x = document.getElementById("easterEgg");
+        x.classList.remove("test");
+    }
 
+    //sonst ist der darkmode nicht aktiv wenn die seite neu geladen wird :(
+    if(darkMode === 'enabled'){
+        enableDarkMode();
     }
 
     darkModeToggle.addEventListener("click", () => {
+        darkMode = localStorage.getItem("darkMode");
         if(darkMode !== "enabled"){
             enableDarkMode();
             console.log(darkMode);
