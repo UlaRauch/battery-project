827a5c9879cbaff913f8a7ad4eae7cfd3d0ed21d
Mattias <42864156+Mattias-R@users.noreply.github.com>
2021-06-24 12:36:25 +0200

    dark mode changes

--- JS/darkmode.js
+++ JS/darkmode.js
@@ -5,16 +5,55 @@
 
     const enableDarkMode = () => {
         localStorage.setItem("darkMode", "enabled");
-        let x = document.getElementById("easterEgg");
-        x.className = "test";
-        //x.style.backgroundColor = "red";
+        let main = document.getElementById("main");
+        main.className = "main-darkmode";
+        main.classList.remove("main-lightmode");
+
+        let body = document.getElementById("body");
+        body.className = "body-darkmode";
+        body.classList.remove("body-lightmode");
+
+        let header = document.getElementById("header");
+        header.className = "nav-darkmode";
+        header.classList.remove("nav-lightmode");
+
+        let mainArticle = document.getElementById("h1");
+        mainArticle.className = "header-darkmode";
+        mainArticle.classList.remove("header-lightmode");
+
+        let footer = document.getElementById("footer");
+        footer.className = "footer-darkmode";
+        footer.classList.remove("footer-lightmode");
+
+
     }
 
     const disableDarkMode = () => {
         localStorage.setItem("darkMode", null);
-        let x = document.getElementById("easterEgg");
-        x.classList.remove("test");
+        let main = document.getElementById("main");
+        main.className = "main-lightmode";
+        main.classList.remove("main-darkmode");
+
+        let body = document.getElementById("body");
+        body.className = "body-lightmode";
+        body.classList.remove("body-darkmode");
+
+        let header = document.getElementById("header");
+        header.className = "nav-lightmode";
+        header.classList.remove("nav-darkmode");
+
+        let mainArticle = document.getElementById("h1");
+        mainArticle.className = "header-lightmode";
+        mainArticle.classList.remove("header-darkmode");
+
+        let footer = document.getElementById("footer");
+        footer.className = "footer-lightmode";
+        footer.classList.remove("footer-darkmode");
+
     }
+
+
+
 
     //sonst ist der darkmode nicht aktiv wenn die seite neu geladen wird :(
     if(darkMode === 'enabled'){
