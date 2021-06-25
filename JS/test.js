54e762604697f6fd715adfaa5fc7dc1c818dea45
Mattias <42864156+Mattias-R@users.noreply.github.com>
2021-06-25 16:52:26 +0200

    added login

---
+++ JS/test.js
@@ -0,0 +1,18 @@
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    console.log("hallo");
+
+    try{
+        let button2 = document.getElementById("orderButton");
+        button2.addEventListener("click", function (){
+            console.log("butoon");
+
+            window.open ("http://localhost:1337",
+                "mywindow","menubar=1,resizable=1,width=500,height=400");
+        })
+    }catch{
+
+    }
+
+});
+
